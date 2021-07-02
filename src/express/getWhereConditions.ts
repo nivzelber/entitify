c79c842f213168d9daed75ef431edeb0c57e6ab4
Niv Zelber <nivznivz0@gamil.com>
2021-07-02 18:05:38 +0300

    extract where conditions logic

---
+++ src/express/getWhereConditions.ts
@@ -0,0 +1,50 @@
+import { ParsedQs } from "qs";
+import { FindOperator } from "typeorm";
+
+import { getGeneralConditions } from "../utils/conditions/generalFields";
+import { getNumbersConditions } from "../utils/conditions/numberFields";
+import { getStringsConditions } from "../utils/conditions/stringFields";
+
+interface GetWhereConditionsProps {
+  query: ParsedQs;
+  stringFields?: string[];
+  numberFields?: string[];
+  and?: boolean;
+}
+
+export const getWhereConditions = ({
+  query,
+  stringFields = [],
+  numberFields = [],
+  and = false
+}: GetWhereConditionsProps) => {
+  let conditions: Record<string, FindOperator<any>>[] = [];
+
+  const allFields = ([] as string[]).concat(stringFields).concat(numberFields);
+  const generalConditions = getGeneralConditions(allFields, query);
+  conditions.push(...generalConditions); // using concat does not work here
+
+  const stringConditions = getStringsConditions(stringFields, query);
+  conditions.push(...stringConditions);
+
+  const numberConditions = getNumbersConditions(numberFields, query);
+  conditions.push(...numberConditions);
+
+  // joining conditions in case and was supplied
+  if (and && conditions.length > 0) {
+    const andConditions: Record<string, FindOperator<any>>[] = [conditions[0]];
+    for (let i = 1; i < conditions.length; i++) {
+      const currentCondition = conditions[i];
+      const fieldName = Object.keys(currentCondition)[0];
+      if (andConditions[0].hasOwnProperty(fieldName)) {
+        andConditions.push(currentCondition);
+      } else {
+        andConditions[0][fieldName] = currentCondition[fieldName];
+      }
+    }
+
+    conditions = andConditions;
+  }
+
+  return conditions;
+};
--- src/express/route.ts
+++ src/express/route.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import { EntityTarget, getConnection, getRepository } from "typeorm";
 
-import { getGeneralConditions } from "../utils/conditions/generalFields";
-import { getNumbersConditions } from "../utils/conditions/numberFields";
-import { getStringsConditions } from "../utils/conditions/stringFields";
 import { getFieldsByType } from "../utils/decode-entity/get-fields-by-type";
 
 import { getQueryString } from "./getQueryString";
+import { getWhereConditions } from "./getWhereConditions";
 
 export interface Options {
   take?: number;
@@ -58,38 +56,15 @@ export const route = <TEntity extends EntityTarget<{ id: number }>>(
         _and = false
       } = getQueryString(req.url);
 
-      //#region find conditions
       const conditions: any = {
-        where: [],
+        where: getWhereConditions({
+          query: req.query,
+          stringFields,
+          numberFields,
+          and: _and as boolean
+        }),
         order: { id: "ASC" }
       };
-
-      const allFields = ([] as string[]).concat(stringFields).concat(numberFields);
-      const generalConditions = getGeneralConditions(allFields, req.query);
-      conditions.where.push(...generalConditions); // using concat does not work here
-
-      const stringConditions = getStringsConditions(stringFields, req.query);
-      conditions.where.push(...stringConditions);
-
-      const numberConditions = getNumbersConditions(numberFields, req.query);
-      conditions.where.push(...numberConditions);
-
-      // joining conditions in case "_and" was supplied
-      if (_and && conditions.where.length > 0) {
-        const andConditions: object[] = [conditions.where[0]];
-        for (let i = 1; i < conditions.where.length; i++) {
-          const currentCondition = conditions.where[i];
-          const fieldName = Object.keys(currentCondition)[0];
-          if (andConditions[0].hasOwnProperty(fieldName)) {
-            andConditions.push(currentCondition);
-          } else {
-            andConditions[0][fieldName] = currentCondition[fieldName];
-          }
-        }
-
-        conditions.where = andConditions;
-      }
-      //#endregion find conditions
 
       if (paginate) {
         conditions.take = take;
