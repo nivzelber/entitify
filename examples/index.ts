d58bdb508fec116b69f53d40681a8d56c4d974fc
Niv Zelber <nivznivz0@gamil.com>
2021-07-02 15:59:06 +0300

    add example files

---
+++ examples/index.ts
@@ -0,0 +1,38 @@
+import cors from "cors";
+import express from "express";
+import { ConnectionOptions, createConnection } from "typeorm";
+
+import { control } from "../src/express";
+
+import { User } from "./user.model";
+
+const start = async () => {
+  const connectionOptions: ConnectionOptions = {
+    type: "mssql",
+    host: "localhost",
+    port: 1434,
+    database: "entitify",
+    username: "Niv54",
+    password: "myCoolDbPassword123",
+    entities: ["dist/examples/**/*.model.js"],
+    synchronize: true,
+    logging: true,
+    logger: "advanced-console",
+    options: {
+      encrypt: false
+    }
+  };
+  await createConnection(connectionOptions);
+
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+  app.use(express.urlencoded({ extended: true }));
+
+  const userController = control(User);
+  app.use("/api/user", userController);
+
+  app.listen(3000, () => console.log("App in running"));
+};
+
+start();
