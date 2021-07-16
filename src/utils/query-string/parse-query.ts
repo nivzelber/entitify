d46a1ecd6de495bf9b5f6a08c50de8fe00bb55b2
Niv Zelber <nivznivz0@gamil.com>
2021-07-16 10:23:51 +0300

    type parse query method

--- src/utils/query-string/parse-query.ts
+++ src/utils/query-string/parse-query.ts
@@ -1,8 +1,8 @@
-import qs from "qs";
+import qs, { ParsedQs } from "qs";
 
 import { queryStringValueDecoder } from "./query-string-value-decoder";
 
-export const parseQuery = (query: string | Record<string, string>) =>
+export const parseQuery = (query: string | Record<string, string>): ParsedQs =>
   qs.parse(query, {
     decoder: queryStringValueDecoder
-  });
+  }) as ParsedQs;
