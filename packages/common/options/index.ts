abb5f85fdedd315d5ae1554d4006016d829aecf7
Niv Zelber <nivznivz0@gamil.com>
2021-09-17 01:33:52 +0300

    add new options

--- packages/common/options/index.ts
+++ packages/common/options/index.ts
@@ -1,6 +1,15 @@
-export interface Options {
+import { BaseEntity } from "@entitify/core";
+
+export interface Options<T = BaseEntity> {
   take?: number;
-  // TODO: add a default sort by field, default for paginate
+  sortBy?: keyof T;
+  sortDirection?: "ASC" | "DESC";
+  paginate?: boolean;
 }
 
-export const defaultOptions: Options = { take: 50 };
+export const defaultOptions: Options = {
+  take: 50,
+  sortBy: "id",
+  sortDirection: "ASC",
+  paginate: true
+};
