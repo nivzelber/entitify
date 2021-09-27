70239159720eb8816d5910cd117efd57eb8df23c
Niv Zelber <nivznivz0@gamil.com>
2021-09-27 03:38:14 +0300

    implement shared logic on nest package

--- packages/nest-rest/general/general.controller.ts
+++ packages/nest-rest/general/general.controller.ts
@@ -1,42 +1,42 @@
-// import { BaseEntity } from "@entitify/common";
-// import { parseQuery } from "@entitify/core";
-// import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from "@nestjs/common";
+import { BaseEntity } from "@entitify/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Req } from "@nestjs/common";
+import { Request } from "express";
 
-// import { CreateGeneralDto } from "./dto/create-general.dto";
-// import { UpdateGeneralDto } from "./dto/update-general.dto";
-// import { GeneralService } from "./general.service";
+import { CreateGeneralDto } from "./dto/create-general.dto";
+import { UpdateGeneralDto } from "./dto/update-general.dto";
+import { GeneralService } from "./general.service";
 
-// @Controller()
-// export class GeneralController {
-//   constructor(private readonly generalService: GeneralService) {}
+@Controller()
+export class GeneralController {
+  constructor(private readonly generalService: GeneralService) {}
 
-//   @Post()
-//   create(@Body() createGeneralDto: CreateGeneralDto<BaseEntity>) {
-//     return this.generalService.create(createGeneralDto.entity);
-//   }
+  @Post()
+  create(@Body() createGeneralDto: CreateGeneralDto<BaseEntity>) {
+    return this.generalService.create(createGeneralDto.entity);
+  }
 
-//   @Get("count")
-//   count() {
-//     return this.generalService.count();
-//   }
+  @Get("count")
+  count(@Req() req: Request) {
+    return this.generalService.count(req.url);
+  }
 
-//   @Get(":id")
-//   findOne(@Param("id") id: string) {
-//     return this.generalService.findOne(+id);
-//   }
+  @Get(":id")
+  findOne(@Param("id") id: string) {
+    return this.generalService.findOne(+id);
+  }
 
-//   @Get()
-//   findByCondition(@Query() query: Record<string, string>) {
-//     return this.generalService.findByCondition(parseQuery(query));
-//   }
+  @Get()
+  findByCondition(@Req() req: Request) {
+    return this.generalService.findByCondition(req.url);
+  }
 
-//   @Patch(":id")
-//   update(@Param("id") id: string, @Body() updateGeneralDto: UpdateGeneralDto) {
-//     return this.generalService.update(+id, updateGeneralDto.entity);
-//   }
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateGeneralDto: UpdateGeneralDto) {
+    return this.generalService.update(+id, updateGeneralDto.entity);
+  }
 
-//   @Delete(":id")
-//   remove(@Param("id") id: string) {
-//     return this.generalService.remove(+id);
-//   }
-// }
+  @Delete(":id")
+  remove(@Param("id") id: string) {
+    return this.generalService.remove(+id);
+  }
+}
